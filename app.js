10049ae7acf42c64fc66ff972d09322969f2b866
tob41ksh <registration@yoshiie.com>
2025-12-03 02:16:56 +0900

    first commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catRouter =require('./routes/cat');
+var report08Router =require('./routes/report08');
 
 var app = express();
 
@@ -21,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cat', catRouter);
+app.use('/report08', report08Router);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
